765e778709758b45766199f0def6a1b7b6838f3e
Ashish Kumar <ashishkr23438@gmail.com>
2021-07-18 20:07:43 +0530

    change vroomdata

--- tools/testers/vroomdata.sql
+++ tools/testers/vroomdata.sql
@@ -17,25 +17,25 @@ CREATE TABLE public.vroom_time_windows (
 INSERT INTO public.vroom_time_windows (
   id, tw_open, tw_close)
   VALUES
-(1, 145000, 175000),
-(2, 50000, 80000),
-(3, 109000, 139000),
-(4, 141000, 171000),
-(5, 41000, 71000),
-(6, 65000, 146000),
-(7, 997000, 1068000),
-(8, 15000, 67000),
-(9, 170000, 225000),
-(10, 621000, 702000),
-(11, 825000, 870000),
-(12, 255000, 324000),
-(13, 357000, 410000),
-(14, 534000, 605000),
-(15, 727000, 782000),
-(16, 10000, 12000),
-(17, 10000, 11000),
+(1, 3625, 4375),
+(2, 1250, 2000),
+(3, 2725, 3475),
+(4, 3525, 4275),
+(5, 1025, 1775),
+(6, 1625, 3650),
+(7, 24925, 26700),
+(8, 375, 1675),
+(9, 4250, 5625),
+(10, 15525, 17550),
+(11, 20625, 21750),
+(12, 6375, 8100),
+(13, 8925, 10250),
+(14, 13350, 15125),
+(15, 18175, 19550),
+(16, 250, 300),
+(17, 250, 275),
 (18, 0, 0),
-(19, 10000, 10000);
+(19, 250, 250);
 -- VROOM TWS TABLE ADD DATA end
 
 
@@ -56,11 +56,11 @@ CREATE TABLE public.vroom_jobs (
 INSERT INTO public.vroom_jobs (
   id, location_index, service, delivery, pickup, skills, priority, time_windows_sql)
   VALUES
-(1, 1, 10000, ARRAY[20], ARRAY[20], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 1$$),
-(2, 2, 10000, ARRAY[30], ARRAY[30], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 2$$),
-(3, 3, 10000, ARRAY[10], ARRAY[10], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 3$$),
-(4, 3, 10000, ARRAY[40], ARRAY[40], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 4$$),
-(5, 4, 10000, ARRAY[20], ARRAY[20], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 5$$);
+(1, 1, 250, ARRAY[20], ARRAY[20], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 1$$),
+(2, 2, 250, ARRAY[30], ARRAY[30], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 2$$),
+(3, 3, 250, ARRAY[10], ARRAY[10], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 3$$),
+(4, 3, 250, ARRAY[40], ARRAY[40], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 4$$),
+(5, 4, 250, ARRAY[20], ARRAY[20], ARRAY[0], 0, $$SELECT * FROM vroom_time_windows WHERE id = 5$$);
 -- VROOM JOBS TABLE ADD DATA end
 
 
@@ -86,20 +86,20 @@ INSERT INTO public.vroom_shipments (
   d_id, d_location_index, d_service, d_time_windows_sql,
   amount, skills, priority)
   VALUES
-(6, 3, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 6$$,
-  7, 5, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 7$$,
+(6, 3, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 6$$,
+  7, 5, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 7$$,
   ARRAY[10], ARRAY[0], 0),
-(8, 5, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 8$$,
-  9, 6, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 9$$,
+(8, 5, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 8$$,
+  9, 6, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 9$$,
   ARRAY[10], ARRAY[0], 0),
-(10, 1, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 10$$,
-  11, 2, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 11$$,
+(10, 1, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 10$$,
+  11, 2, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 11$$,
   ARRAY[20], ARRAY[0], 0),
-(12, 1, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 12$$,
-  13, 4, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 13$$,
+(12, 1, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 12$$,
+  13, 4, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 13$$,
   ARRAY[20], ARRAY[0], 0),
-(14, 2, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 14$$,
-  15, 2, 90000, $$SELECT * FROM vroom_time_windows WHERE id = 15$$,
+(14, 2, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 14$$,
+  15, 2, 2250, $$SELECT * FROM vroom_time_windows WHERE id = 15$$,
   ARRAY[10], ARRAY[0], 0);
 -- VROOM SHIPMENTS TABLE ADD DATA end
 
@@ -133,19 +133,19 @@ CREATE TABLE public.vroom_vehicles (
   tw_open INTEGER,
   tw_close INTEGER,
   breaks_sql TEXT,
-  steps_sql TEXT
+  speed_factor FLOAT
 );
 -- VROOM VEHICLES TABLE CREATE end
 
 -- VROOM VEHICLES TABLE ADD DATA start
 INSERT INTO public.vroom_vehicles (
   id, start_index, end_index, capacity, skills,
-  tw_open, tw_close, breaks_sql, steps_sql)
+  tw_open, tw_close, breaks_sql, speed_factor)
   VALUES
-(1, 1, 1, ARRAY[200], ARRAY[0], 0, 1236000, $$SELECT * FROM vroom_breaks WHERE id = 16$$, NULL),
-(2, 1, 3, ARRAY[200], ARRAY[0], 3600, 1236000, $$SELECT * FROM vroom_breaks WHERE id = 17$$, NULL),
-(3, 1, 1, ARRAY[200], ARRAY[0], 0, 1236000, $$SELECT * FROM vroom_breaks WHERE id = 18$$, NULL),
-(4, 3, 3, ARRAY[200], ARRAY[0], 0, 1236000, $$SELECT * FROM vroom_breaks WHERE id = 19$$, NULL);
+(1, 1, 1, ARRAY[200], ARRAY[0], 0, 30900, $$SELECT * FROM vroom_breaks WHERE id = 16$$, 1.0),
+(2, 1, 3, ARRAY[200], ARRAY[0], 100, 30900, $$SELECT * FROM vroom_breaks WHERE id = 17$$, 1.0),
+(3, 1, 1, ARRAY[200], ARRAY[0], 0, 30900, $$SELECT * FROM vroom_breaks WHERE id = 18$$, 1.0),
+(4, 3, 3, ARRAY[200], ARRAY[0], 0, 30900, $$SELECT * FROM vroom_breaks WHERE id = 19$$, 1.0);
 -- VROOM VEHICLES TABLE ADD DATA end
 
 -- VROOM MATRIX TABLE CREATE start
@@ -160,10 +160,10 @@ CREATE TABLE public.vroom_matrix (
 INSERT INTO public.vroom_matrix (
   start_vid, end_vid, agg_cost)
   VALUES
-(1, 1, 0), (1, 2, 2000), (1, 3, 3606), (1, 4, 3000), (1, 5, 4243), (1, 6, 5099),
-(2, 1, 2000), (2, 2, 0), (2, 3, 5000), (2, 4, 3606), (2, 5, 5831), (2, 6, 5099),
-(3, 1, 3606), (3, 2, 5000), (3, 3, 0), (3, 4, 2000), (3, 5, 1000), (3, 6, 3606),
-(4, 1, 3000), (4, 2, 3606), (4, 3, 2000), (4, 4, 0), (4, 5, 3000), (4, 6, 2236),
-(5, 1, 4243), (5, 2, 5831), (5, 3, 1000), (5, 4, 3000), (5, 5, 0), (5, 6, 4472),
-(6, 1, 5099), (6, 2, 5099), (6, 3, 3606), (6, 4, 2236), (6, 5, 4472), (6, 6, 0);
+(1, 1, 0), (1, 2, 50), (1, 3, 90), (1, 4, 75), (1, 5, 106), (1, 6, 127),
+(2, 1, 50), (2, 2, 0), (2, 3, 125), (2, 4, 90), (2, 5, 145), (2, 6, 127),
+(3, 1, 90), (3, 2, 125), (3, 3, 0), (3, 4, 50), (3, 5, 25), (3, 6, 90),
+(4, 1, 75), (4, 2, 90), (4, 3, 50), (4, 4, 0), (4, 5, 75), (4, 6, 55),
+(5, 1, 106), (5, 2, 145), (5, 3, 25), (5, 4, 75), (5, 5, 0), (5, 6, 111),
+(6, 1, 127), (6, 2, 127), (6, 3, 90), (6, 4, 55), (6, 5, 111), (6, 6, 0);
 -- VROOM MATRIX TABLE ADD DATA end
